7a3139548580eaa20d003a77b9269845fd4ba5e2
unknown <alnosila.abd@gmail.com>
2021-01-19 10:35:45 +0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { TodosService } from '../shared/todos.service';
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  private loading: boolean = true
+
+  constructor(private todosService: TodosService) { }
+
+  ngOnInit(){
+
+    this.todosService.fetchTodos().subscribe( () => {
+      this.loading = false
+    })
+  }
+
+  onChange(id: number){
+    this.todosService.onToggle(id);
+  }
+  removeTodo(id: number){
+    this.todosService.removeTodo(id);
+  }
+}
